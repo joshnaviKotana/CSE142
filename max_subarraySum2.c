10583a92c20df06854a8d7c2985ff2165ded439e
joshnaviKotana <josh.kotana12@gmail.com>
2026-07-05 22:05:11 +0530

    Add files via upload
    Maximum Subarray sum using 2 loops(Better Solution).

---
+++ max_subarraySum2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+int n,sum=0,maxSum=0;
+printf("Enter size of an array:");
+scanf("%d",&n);
+int arr[n];
+printf("Enter the elements:");
+for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+for(int i=0;i<n;i++)
+{
+for(int j=i;j<n;j++)
+{
+  for(int k=i;k<=j;k++)
+  {
+   sum+=arr[k];
+   }
+   if(sum>maxSum) maxSum=sum;
+   sum=0;
+}
+}
+printf("%d\n",maxSum);
+return 0;
+}
